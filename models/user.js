e26c2896f1ab9d44d33cfacb3fff78eae03b816e
Bojan Peric <boki@iMac-141.local>
2021-06-22 17:27:51 +0200

    .

--- models/user.js
+++ models/user.js
@@ -1,54 +1,94 @@
-const mongoose = require('mongoose')
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
+const crypto = require("crypto");
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const UserSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, 'Please add name']
-    },
-    email: {
-        type: String,
-        required: [true, 'Please add email']
-    },
-    password: {
-        type: String,
-        required: [true, 'Please add password'],
-        minlength: [6, 'Password must be at least 6 characters long'],
-        select: false
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
-    }
-})
-
+  name: {
+    type: String,
+    required: [true, "Please add name"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please add email"],
+    unique: true,
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      "Please add a valid email",
+    ],
+  },
+  password: {
+    type: String,
+    required: [true, "Please add password"],
+    minlength: [6, "Password must be at least 6 characters long"],
+    select: false,
+  },
+  resetPasswordToken: String,
+  resetPasswordExpire: Date,
+  confirmEmailToken: String,
+  isEmailConfirmed: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
 //Encrypt password using bcrypt
 //static method called on the model
-UserSchema.pre('save', async function (next) {
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
 
-    if (!this.isModified('password')) {
-        next()
-    }
-
-    const salt = await bcrypt.genSalt(10)
-    this.password = await bcrypt.hash(this.password, salt)
-
-})
-
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 
 //sign jwt and return it
 UserSchema.methods.getSignedJwtToken = function () {
-    return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
-        expiresIn: process.env.JWT_EXPIRE   
-    })
-}
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE,
+  });
+};
 
 //match user password to hashed password in db
 UserSchema.methods.matchPassword = async function (enteredPassword) {
-    return await bcrypt.compare(enteredPassword, this.password)
-}
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
+// Generate and hash password token
+UserSchema.methods.getResetPasswordToken = function () {
+  // Generate token
+  const resetToken = crypto.randomBytes(20).toString("hex");
 
-module.exports = mongoose.model('User', UserSchema)
+  // Hash token and set to resetPasswordToken field
+  this.resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
+
+  // Set expire
+  this.resetPasswordExpire = Date.now() + 10 * 60 * 1000;
+
+  return resetToken;
+};
+
+// Generate email confirm token
+UserSchema.methods.generateEmailConfirmToken = function (next) {
+  // email confirmation token
+  const confirmationToken = crypto.randomBytes(20).toString("hex");
+
+  this.confirmEmailToken = crypto
+    .createHash("sha256")
+    .update(confirmationToken)
+    .digest("hex");
+
+  const confirmTokenExtend = crypto.randomBytes(100).toString("hex");
+  const confirmTokenCombined = `${confirmationToken}.${confirmTokenExtend}`;
+  return confirmTokenCombined;
+};
+
+module.exports = mongoose.model("User", UserSchema);
--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,25 @@
-const express = require('express')
-const {register, login} = require('../controllers/auth')
+const express = require("express");
+const {protect} = require("../middleware/auth");
+const {
+  register,
+  login,
+  confirmEmail,
+  forgotPassword,
+  resetPassword,
+  getMe,
+  updateDetails,
+  updatePassword
+} = require("../controllers/auth");
 
+const router = express.Router();
 
-const router = express.Router()
+router.route("/register").post(register);
+router.route("/login").post(login);
+router.route("/confirmemail").get(confirmEmail);
+router.route("/me").get(protect, getMe);
+router.route("/updatedetails").put(protect, updateDetails);
+router.route("/forgotpassword").post(forgotPassword);
+router.route("/resetpassword/:resettoken").put(resetPassword);
+router.route("/updatepassword").put(protect, updatePassword);
 
-router.route('/register').post(register)
-router.route('/login').post(login)
-
-
-module.exports = router
+module.exports = router;
